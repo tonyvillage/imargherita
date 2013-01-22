2cc319bba0d7486b4eb363788f55e20e3edebe29
Sebastiano <s.ronzulli@gmail.com>
2013-01-22 16:54:01 +0100

    Initial Commit

---
+++ iMargherita/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iMargherita
+//
+//  Created by Sebastiano Ronzulli on 22/01/13.
+//  Copyright (c) 2013 Sebastiano Ronzulli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
